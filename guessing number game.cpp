e02ca11509394dcb8f07421abf83ee597334fa40
Raunak Gupta <106958764+raunak87233@users.noreply.github.com>
2022-12-18 22:09:55 +0530

    Update guessing number game.cpp

--- guessing number game.cpp
+++ guessing number game.cpp
@@ -9,21 +9,33 @@ int main()
 	srand(time(0)); //seed random number generator
 	num = rand() % 100 + 1; // random number between 1 and 100
 	cout << "Guess My Number Game\n\n";
+	
 
 	do
 	{
 		cout << "Enter a guess between 1 and 100 : ";
 		cin >> guess;
 		tries++;
-
+string stri =to_string(tries);
+if(tries==8)
+	{
+	    cout<<"you lose\n\n";
+	}
 		if (guess > num)
+		{
 			cout << "Too high!\n\n";
+			cout << stri<<endl ;
+		}
 		else if (guess < num)
+		{
 			cout << "Too low!\n\n";
+			cout << stri<<endl ;
+		}
 		else
+		{
 			cout << "\nCorrect! You got it in " << tries << " guesses!\n";
+		}
 	} while (guess != num);
 
 	return 0;
 }
-
