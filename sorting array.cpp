3d9c40d53752e984e1f28c84e16d61c425ffe2ed
Raunak Gupta <106958764+raunak87233@users.noreply.github.com>
2022-12-27 10:08:05 +0530

    Update sorting array.cpp

--- sorting array.cpp
+++ sorting array.cpp
@@ -6,17 +6,30 @@ void printVector(vector<int> A){
         cout<<endl;
     }
 }
+int getIndexOFminelementitoend(vector<int> A, int i){
+    int n=A.size();
+    int minIndex=i;
+    for(int j=i+1; j<n; j++){
+        if(A[j]<A[minIndex]){
+            minIndex=j;
+        }
+}
+return minIndex;
+}
 void sort(vector<int>&A){
     for(int i=0; i<A.size(); i++){
-        int j= (A,i);
-        swap(A[i], A[j]);
-        printf("vector(A)");
-        cout<<"i"<<i<<"array";
+        
+int j=getIndexOFminelementitoend(A,i);
+swap(A[i],A[j]);
     }
 };
 int main(){
     vector<int>A={5,2,4,7,8,3,1,9};
     sort(A);
-     printf ("vector(A)");
+     
     cout<<"sorted";
+    printVector(A);
+    return 0;
 }
+
+
